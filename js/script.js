995ecb9b7b97c3d28edfeeb98b1d0294b61c27cd
Polozova Lena <e.polozova96@gmail.com>
2023-10-20 17:54:36 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,10 +10,10 @@ const personalMovieDB = {
     private: false
 };
 
-const nameMovies1 = prompt("Один из последних просмотренных фильмов?", ""),
-      ratingMovies1 = prompt("Насколько оцените его?", ""),
-      nameMovies2 = prompt("Один из последних просмотренных фильмов?", ""),
-      ratingMovies2 = prompt("Насколько оцените его?", "");
+const nameMovies1 = prompt('Один из последних просмотренных фильмов?', ''),
+      ratingMovies1 = prompt('Насколько оцените его?', ''),
+      nameMovies2 = prompt('Один из последних просмотренных фильмов?', ''),
+      ratingMovies2 = prompt('Насколько оцените его?', '');
 
 personalMovieDB.movies[nameMovies1] = ratingMovies1;
 personalMovieDB.movies[nameMovies2] = ratingMovies2;
