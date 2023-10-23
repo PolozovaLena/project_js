b50e9636bb057a1e776f7c064bafbb92e29d5b36
Polozova Lena <e.polozova96@gmail.com>
2023-10-23 09:44:48 +0300

    update homework

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
+let nameMovies, 
+    ratingMovies;
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -10,12 +11,25 @@ const personalMovieDB = {
     private: false
 };
 
-const nameMovies1 = prompt('Один из последних просмотренных фильмов?', ''),
-      ratingMovies1 = prompt('Насколько оцените его?', ''),
-      nameMovies2 = prompt('Один из последних просмотренных фильмов?', ''),
-      ratingMovies2 = prompt('Насколько оцените его?', '');
+for(let i = 0; i < 2; i++) {
+    nameMovies = prompt('Один из последних просмотренных фильмов?', ''),
+    ratingMovies = prompt('Насколько оцените его?', '');
 
-personalMovieDB.movies[nameMovies1] = ratingMovies1;
-personalMovieDB.movies[nameMovies2] = ratingMovies2;
+    if (nameMovies.length != 0 && nameMovies.length != '' && nameMovies.length < 50 && ratingMovies.length !=0 && ratingMovies.length != '') {
+        personalMovieDB.movies[nameMovies] = ratingMovies;
+    } else {
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
