ecbdeea16cccb0c06b1ac08de8e04448ec4fd483
Polozova Lena <e.polozova96@gmail.com>
2023-11-02 08:15:03 +0300

    update homework v4

--- js/script.js
+++ js/script.js
@@ -1,68 +1,67 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-start();
-
-let nameMovies, 
-    ratingMovies;
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
-
-function rememberMyFilms() {
-    for(let i = 0; i < 2; i++) {
-        nameMovies = prompt('Один из последних просмотренных фильмов?', ''),
-        ratingMovies = prompt('Насколько оцените его?', '');
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
     
-        if (nameMovies.length != 0 && nameMovies.length != '' && nameMovies.length < 50 && ratingMovies.length !=0 && ratingMovies.length != '') {
-            personalMovieDB.movies[nameMovies] = ratingMovies;
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function() {
+        for(let i = 0; i < 2; i++) {
+            const nameMovies = prompt('Один из последних просмотренных фильмов?', '').trim(),
+                  ratingMovies = prompt('Насколько оцените его?', '');
+        
+            if (nameMovies.length != 0 && nameMovies.length != '' && nameMovies.length < 50 && ratingMovies.length !=0 && ratingMovies.length != '') {
+                personalMovieDB.movies[nameMovies] = ratingMovies;
+            } else {
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log('Просмотрено довольно мало фильмов');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+            console.log('Вы классический зритель');
+        } else if (personalMovieDB >= 30) {
+            console.log('Вы киноман');
         } else {
-            i--;
+            console.log('Произошла ошибка');
+        }
+    },
+    writeYourGenres: function() {
+        for(let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+            
+            if (genre == null || genre === '') {
+                console.log("Вы ввели некорректные данные или не ввели их вовсе")
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+        }
+
+        personalMovieDB.genres.forEach((value, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${value}`);
+        });
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        } 
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB = true;
         }
     }
-}
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
-        console.log('Вы классический зритель');
-    } else if (personalMovieDB >= 30) {
-        console.log('Вы киноман');
-    } else {
-        console.log('Произошла ошибка');
-    }
-}
-
-detectPersonalLevel();
-
-function writeYourGenres() {
-    for(let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-
-writeYourGenres();
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    } 
-}
-
-showMyDB(personalMovieDB.privat);
+};
